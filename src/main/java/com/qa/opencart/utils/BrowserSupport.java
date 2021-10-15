bc8e0ca637aa38069e6b0016c112811e153b108a
ujwal <ujwaldpr@gmail.com>
2021-10-15 14:40:31 +0530

    main branch

--- src/main/java/com/qa/opencart/utils/BrowserSupport.java
+++ src/main/java/com/qa/opencart/utils/BrowserSupport.java
@@ -4,6 +4,9 @@ public class BrowserSupport {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("chrome");
+		System.out.println("firefox");
 
 	}
 
