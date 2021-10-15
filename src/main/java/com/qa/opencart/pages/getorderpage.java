e4d8356c9052ab230ac838fe670e9feca6689e30
ujwal <ujwaldpr@gmail.com>
2021-10-15 23:51:00 +0530

    Order page

---
+++ src/main/java/com/qa/opencart/pages/getorderpage.java
@@ -0,0 +1,20 @@
+package com.qa.opencart.pages;
+
+public class getorderpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Get the main method of the order page");
+		getorder();
+
+	}
+
+	
+	public static void getorder(){
+		
+		System.out.println("This is the method of the get order page");
+		
+		
+	}
+}
